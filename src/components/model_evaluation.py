c52a8aa2bc0c04bd34294a37710b46186a42db95
dasaditya1999 <dasaditya576@gmail.com>
2024-04-14 11:06:34 +0530

    Updating the Model Evaluation & Training-pipeline codebase

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -13,30 +13,30 @@ from src.exception. exception import customexception
 class ModelEvaluation:
     def __init__(self):
         logging.info("evaluation started")
+        print('evaluation started')
 
     def eval_metrics(self,actual,pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
         mae = mean_absolute_error(actual, pred)# here is MAE
-        r2 = r2_score(actual, pred)# here is r3 value
+        r2 = r2_score(actual, pred)# here is r2 value
         logging.info("evaluation metrics captured")
         return rmse, mae, r2
 
     def initiate_model_evaluation(self,train_array,test_array):
+        print('hello****')
         try:
              X_test,y_test=(test_array[:,:-1], test_array[:,-1])
 
              model_path=os.path.join("artifacts","model.pkl")
              model=load_object(model_path)
 
-             #mlflow.set_registry_uri("")
+            # mlflow.set_registry_uri("") # Developer - Provide path of the Cloud Repository.
              
-             logging.info("model has register")
+             logging.info("Lasso model has registered")
 
              tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
 
-             print(tracking_url_type_store)
-
-
+             print('tracking uri is here',tracking_url_type_store)
 
              with mlflow.start_run():
 
@@ -46,11 +46,11 @@ class ModelEvaluation:
 
                 mlflow.log_metric("rmse", rmse)
                 mlflow.log_metric("r2", r2)
-                mlflow.log_metric("mae", mae)
+                # mlflow.log_metric("mae", mae)
+                # mlflow.log_metric('sample', rmse,r2,mae)
 
                  # Model registry does not work with file store
                 if tracking_url_type_store != "file":
-
                     # Register the model
                     # There are other ways to use the Model Registry, which depends on the use case,
                     # please refer to the doc for more information:
@@ -58,7 +58,5 @@ class ModelEvaluation:
                     mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
                 else:
                     mlflow.sklearn.log_model(model, "model")
-
-
         except Exception as e:
             raise customexception(e,sys)
